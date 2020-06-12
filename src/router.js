c21b7998faa602d956976e6af640d4c14fbf11f8
liyouxiu <1843982989@qq.com>
2020-06-12 18:18:22 +0800

    完成登录

---
+++ src/router.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Login from './cpns/Login.vue'
+import Home from './cpns/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: './login'
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
+    component: Home
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router
